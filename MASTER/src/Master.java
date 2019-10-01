f6f8faacb08cd672b49ccda40fef27b519187d41
Thomas <thomas.koch.ea2011@gmail.com>
2019-10-01 11:22:14 +0200

    Ajout des dernières versions des programmes Java

---
+++ MASTER/src/Master.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class Master {
+
+	public static void main(String[] args) throws IOException, InterruptedException { 
+
+		int timeout = Integer.parseInt(args[0]); // pour tester le programme, aller dans Run->Run configurations->Onglet Arguments->rentrer le nombre du timeout voulu
+
+		ProcessBuilder pb = new ProcessBuilder("java", "-jar", "/tmp/tkoch/SLAVE.jar"); // ou dans le répertoire /home/p5hngk/Downloads/SLAVE.jar
+		pb.inheritIO();
+		Process proc = pb.start();
+		boolean ret = proc.waitFor(timeout, TimeUnit.SECONDS); // permet de renvoyer true ou false pour savoir si le timeout est arrivé au bout avant que le process ne soit fini ou pas
+		if (ret) {
+			System.out.println("Pas de timeout, le process est allé au bout");
+		}
+		else {
+			System.out.println("Timeout, le process n'est pas allé au bout");
+			proc.destroy();
+		}
+		// pb.redirectError();
+		// pb.redirectErrorStream(true);
+		// pb.redirectOutput();
+
+	}
+}
+
+
---
+++ SLAVE/src/Slave.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Slave {
+
+	public static void main(String args[]) throws IOException, InterruptedException {
+		int a = 3+5 ;
+		Thread.sleep(10000);
+        System.err.println(a);
+    	
+	}
+}
